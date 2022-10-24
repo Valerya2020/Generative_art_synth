9d0779d286a07e6da2924b5396d0286f1c224c68
Valerya2020 <svr2014@mail.ru>
2022-10-24 13:29:04 +0300

    main

---
+++ db/seeds.rb
@@ -0,0 +1,81 @@
+@prototypes_data = [
+  {
+    name: 'First module / First prototype'
+  },
+  {
+    name: 'First oscillator'
+    # ruby hash - это тип данных
+  },
+  {
+    name: 'Abstraction / homework'
+  },
+  {
+    name: 'Oscillator with react components'
+    # ruby hash - это тип данных
+  },
+  {
+    name: 'Tone js'
+    # ruby hash - это тип данных
+  },
+  {
+    name: 'Effects homework'
+    # ruby hash - это тип данных
+  },
+  {
+    name: 'Effects homework 2'
+    # ruby hash - это тип данных
+  },
+  {
+    name: 'Effects homework 3'
+    # ruby hash - это тип данных
+  },
+  {
+    name: 'UI'
+    # ruby hash - это тип данных
+  },
+  {
+    name: 'UI better'
+    # ruby hash - это тип данных
+  },
+  {
+    name: 'UI homework'
+  },
+  {
+    name: 'UI homework 2'
+  },
+  {
+    name: 'Composition'
+  },
+  {
+    name: '1st module synth part 1'
+  },
+  {
+    name: '1st module synth part 2'
+  },
+  {
+    name: '1st module synth part 3 rename'
+  },
+  {
+    name: '1st module synth part 4 radiationChannel'
+  }
+]
+
+def seed
+  reset_db
+  create_prototypes
+end
+
+def reset_db
+  Rake::Task['db:drop'].invoke
+  Rake::Task['db:create'].invoke
+  Rake::Task['db:migrate'].invoke
+end
+
+def create_prototypes
+  @prototypes_data.each do |prototype_data|
+    prototype = Prototype.create!(prototype_data)
+    puts "#{prototype.name} prototype just created"
+  end
+end
+
+seed
