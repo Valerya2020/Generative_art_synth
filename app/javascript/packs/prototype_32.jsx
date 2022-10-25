f67c636c5da11acdea736e138a98b3ca8696ae0e
Valerya2020 <svr2014@mail.ru>
2022-10-26 02:36:08 +0300

    main

---
+++ app/javascript/packs/prototype_32.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_32/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_32')
+  ReactDOM.render(<Container />, container)
+})
