e04b0c7741aeb727274485a7c55896fd360e94b1
Valerya2020 <svr2014@mail.ru>
2022-10-26 01:11:17 +0300

    main

---
+++ app/javascript/packs/prototype_28.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_28/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_28')
+  ReactDOM.render(<Container />, container)
+})
---
+++ app/javascript/packs/prototype_29.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_29/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_29')
+  ReactDOM.render(<Container />, container)
+})
