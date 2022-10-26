c2343c4771046cc4f393584e1f21c92246c61ca7
Valerya2020 <svr2014@mail.ru>
2022-10-27 00:55:11 +0300

    main

---
+++ app/javascript/packs/prototype_38.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_38/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_38')
+  ReactDOM.render(<Container />, container)
+})
