bca7275efdfd9068ae8cc181d9107edb70544bdf
Valerya2020 <svr2014@mail.ru>
2022-12-27 01:33:26 +0300

    main

---
+++ app/javascript/packs/prototype_52.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_52/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_52')
+  ReactDOM.render(<Container />, container)
+})
---
+++ app/javascript/packs/prototype_54.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_54/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_54')
+  ReactDOM.render(<Container />, container)
+})
