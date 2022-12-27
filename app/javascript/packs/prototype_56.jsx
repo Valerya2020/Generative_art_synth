73fef19749678a990eaf4c2fbfd822e325c2fb47
Valerya2020 <svr2014@mail.ru>
2022-12-27 20:15:05 +0300

    main

---
+++ app/javascript/packs/prototype_56.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Container from '../prototypes/prototype_56/Container'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('prototype_56')
+  ReactDOM.render(<Container />, container)
+})
